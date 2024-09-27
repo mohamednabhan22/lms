5c812b15a9b04594647c572e382f9aaab22729dc
hitham07 <hitamnabhanextra@gmail.com>
2024-09-27 11:28:57 +0300

    clerk auth + layout

---
+++ app/(dashboard)/_components/sidebar-routes.tsx
@@ -0,0 +1,30 @@
+'use client'
+import { Compass, Layout } from "lucide-react"
+import { SidebarItem } from "./sidebar-item"
+
+const guestRoutes=[{
+    icon:Layout,
+    label:'Dasboard',
+    href:'/',
+    },{
+icon:Compass,
+label:'Browse',
+href:'/search',
+}]
+export const SidebarRoutes = () => {
+    const routes=guestRoutes
+    return (
+        <div className="flex flex-col w-full">
+{routes.map((route)=>(
+    <SidebarItem
+    key={route.href}
+    icon={route.icon}
+    label={route.label}
+    href={route.href}
+    />
+))}
+        </div>
+    
+    )    
+}
+ 
